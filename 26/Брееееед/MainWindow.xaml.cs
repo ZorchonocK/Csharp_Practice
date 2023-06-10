78f1bc1af79f126a68bf15d9e2476f7d9cacbbe2
Брэдуляк Антон <69217084+ZorchonocK@users.noreply.github.com>
2023-06-10 10:07:07 +0300

    Add files via upload

---
+++ 26/Брееееед/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Брееееед
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int a = Convert.ToInt32(txt1.Text);
+                if (a == 1 || a == 2 || a == 12)
+                {
+                    txt2.Text = "Зима";
+                }
+                else if (a == 3  || a == 4 || a == 5) 
+                {
+                    txt2.Text = "Весна";
+                }
+                else if (a == 6 || a == 7 || a == 8)
+                {
+                    txt2.Text = "Лето";
+                }
+                else if (a == 9 || a == 10 || a == 11)
+                {
+                    txt2.Text = "Осень";
+                }
+                else
+                {
+
+                    MessageBox.Show("Введеные не верные данные");
+                    txt2.Text = "";
+                }
+            }
+            catch (Exception)
+            {
+
+                 MessageBox.Show("Введеные не верные данные");
+                txt2.Text = "";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int x = Convert.ToInt32(txt3.Text);
+                double y = (Math.Abs(Math.Pow(x, 2) - Math.Pow(x, 3))) - (7 * x) / (Math.Pow(x, 3) - 15 * x);
+                txt4.Text = y.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Введеные не верные данные");
+            }
+        }
+    }
+}
---
+++ 27/27/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _27
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textIn1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string inputString = textIn1.Text;
+            int count = 0;
+
+            foreach (char c in inputString)
+            {
+                if (Char.IsDigit(c))
+                {
+                    count++;
+                }
+            }
+
+            textOut1.Content = "Кол-во цифр: " + count;
+        }
+
+
+
+
+        public int[,] myArray;
+
+        public void CreateArray(int row1, int row2)
+        {
+            myArray = new int[row1, row2];
+            Random rnd = new Random();
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    myArray[i, j] = rnd.Next(1, 101);
+                }
+            }
+        }
+
+        public void SwapRows(int[,] array)
+        {
+            int columns = myArray.GetLength(1);
+
+            for (int j = 0; j < columns; j++)
+            {
+                int temp = array[2, j];
+                array[2, j] = array[4, j];
+                array[4, j] = temp;
+            }
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CreateArray(6, 4);
+
+            DataTable dataTable = new DataTable();
+
+            for (int i = 0; i < myArray.GetLength(1); i++)
+            {
+                dataTable.Columns.Add($"Column {i + 1}", typeof(int));
+            }
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                DataRow row = dataTable.NewRow();
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    row[j] = myArray[i, j];
+                }
+                dataTable.Rows.Add(row);
+            }
+
+            arrayGrid.ItemsSource = dataTable.DefaultView;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SwapRows(myArray);
+
+
+            DataTable dataTable = new DataTable();
+
+            for (int i = 0; i < myArray.GetLength(1); i++)
+            {
+                dataTable.Columns.Add($"Column {i + 1}", typeof(int));
+            }
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                DataRow row = dataTable.NewRow();
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    row[j] = myArray[i, j];
+                }
+                dataTable.Rows.Add(row);
+            }
+
+            arrayGrid.ItemsSource = dataTable.DefaultView;
+        }
+
+    }
+}
