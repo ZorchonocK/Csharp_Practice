cd292aa28e1156c1e006b214fd9d40561c1dc37f
Zorchonock <antonikop84@gmail.com>
2023-06-10 10:47:07 +0300

    Add files

---
+++ 30/30/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using HelixToolkit.Wpf;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Media3D;
+
+namespace _30
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            load3dModel();            
+        }
+        public void load3dModel()
+        {
+            ObjReader CurrentHelixObjReader = new ObjReader();
+            Model3DGroup MyModel = CurrentHelixObjReader.Read(@"C:\Практика\30\30\bin\Debug\rose.obj");
+            model.Content = MyModel;
+
+            viewport.Children.Add(new DefaultLights());
+
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                From = 0,
+                To = 40,
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever,
+                Duration = TimeSpan.FromSeconds(2)
+            };
+
+            AxisAngleRotation3D rotation = new AxisAngleRotation3D(new Vector3D(0, 1, 0), 0);
+            RotateTransform3D rotateTransform = new RotateTransform3D(rotation);
+            model.Transform = rotateTransform;
+            rotation.BeginAnimation(AxisAngleRotation3D.AngleProperty, animation);
+
+        }
+    }
+}
---
+++ 31/31/Program.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+
+namespace MuseumDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string databasePath = "museum.db";
+
+            using (var connection = new SQLiteConnection($"Data Source={databasePath}"))
+            {
+                connection.Open();
+
+                // Создание таблиц
+                using (var command = new SQLiteCommand("CREATE TABLE IF NOT EXISTS Сотрудники (Id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Position TEXT, ExcursionId INTEGER, FOREIGN KEY (ExcursionId) REFERENCES Экскурсии (Id))", connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = new SQLiteCommand("CREATE TABLE IF NOT EXISTS Экскурсии (Id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Date DATE)", connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = new SQLiteCommand("CREATE TABLE IF NOT EXISTS Экспонаты (Id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Description TEXT, RoomId INTEGER, FOREIGN KEY (RoomId) REFERENCES Залы (Id))", connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = new SQLiteCommand("CREATE TABLE IF NOT EXISTS Залы (Id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Description TEXT)", connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                //---------------------------------------------------//
+
+
+
+                //Добавление данных в таблицу
+                using (var command = new SQLiteCommand("INSERT INTO Сотрудники (Name, Position, ExcursionId) VALUES (@Name, @Position, @ExcursionId)", connection))
+                {
+                    command.Parameters.AddWithValue("@Name", "John Doe");
+                    command.Parameters.AddWithValue("@Position", "Curator");
+                    command.Parameters.AddWithValue("@ExcursionId", 1);
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = new SQLiteCommand("INSERT INTO Экскурсии (Name, Date) VALUES (@Name, @Date)", connection))
+                {
+                    command.Parameters.AddWithValue("@Name", "Ancient History");
+                    command.Parameters.AddWithValue("@Date", DateTime.Parse("2023-06-15"));
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = new SQLiteCommand("INSERT INTO Экспонаты (Name, Description, RoomId) VALUES (@Name, @Description, @RoomId)", connection))
+                {
+                    command.Parameters.AddWithValue("@Name", "Egyptian Statue");
+                    command.Parameters.AddWithValue("@Description", "Ancient statue from Egypt");
+                    command.Parameters.AddWithValue("@RoomId", 1);
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = new SQLiteCommand("INSERT INTO Залы (Name, Description) VALUES (@Name, @Description)", connection))
+                {
+                    command.Parameters.AddWithValue("@Name", "Ancient Art");
+                    command.Parameters.AddWithValue("@Description", "Exhibit of ancient artworks");
+                    command.ExecuteNonQuery();
+                }
+
+                //---------------------------------------------------//
+
+
+
+                // Вывод содержимого таблиц
+                using (var command = new SQLiteCommand("SELECT Сотрудники.Id, Сотрудники.Name, Сотрудники.Position, Экскурсии.Name AS ExcursionName FROM Сотрудники LEFT JOIN Экскурсии ON Сотрудники.ExcursionId = Экскурсии.Id", connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine("Сотрудники:");
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string name = reader.GetString(1);
+                            string position = reader.GetString(2);
+                            string excursionName = reader.IsDBNull(3) ? "No Excursion" : reader.GetString(3);
+                            Console.WriteLine($"Id: {id}, Name: {name}, Position: {position}, Excursion: {excursionName}");
+                        }
+                    }
+                }
+
+                using (var command = new SQLiteCommand("SELECT * FROM Экскурсии", connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine("\nЭкскурсии:");
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string name = reader.GetString(1);
+                            DateTime date = reader.GetDateTime(2);
+                            Console.WriteLine($"Id: {id}, Name: {name}, Date: {date}");
+                        }
+                    }
+                }
+
+                using (var command = new SQLiteCommand("SELECT Экспонаты.Id, Экспонаты.Name, Экспонаты.Description, Залы.Name AS RoomName FROM Экспонаты LEFT JOIN Залы ON Экспонаты.RoomId = Залы.Id", connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine("\nЭкспонаты:");
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string name = reader.GetString(1);
+                            string description = reader.GetString(2);
+                            string roomName = reader.IsDBNull(3) ? "No Room" : reader.GetString(3);
+                            Console.WriteLine($"Id: {id}, Name: {name}, Description: {description}, Room: {roomName}");
+                        }
+                    }
+                }
+
+                using (var command = new SQLiteCommand("SELECT * FROM Залы", connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine("\nЗалы:");
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string name = reader.GetString(1);
+                            string description = reader.GetString(2);
+                            Console.WriteLine($"Id: {id}, Name: {name}, Description: {description}");
+                        }
+                    }
+                }
+                //---------------------------------------------------//
+                Console.WriteLine("\nХотите произвести действие?");
+                Console.WriteLine("1. Добавление");
+                Console.WriteLine("2. Удаление");
+                Console.WriteLine("3. Нет");
+
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("Введите имя сотрудника");
+                        string newname = Console.ReadLine();
+                        Console.WriteLine("Введите роль сотрудника");
+                        string newposition = Console.ReadLine();
+
+                        // Добавление элемента в таблицу "Сотрудники"
+                        using (var command = new SQLiteCommand("INSERT INTO Сотрудники (Name, Position, ExcursionId) VALUES (@Name, @Position, @ExcursionId)", connection))
+                        {
+                            command.Parameters.AddWithValue("@Name", newname);
+                            command.Parameters.AddWithValue("@Position", newposition);
+                            command.Parameters.AddWithValue("@ExcursionId", 1); // Идентификатор связанной экскурсии
+                            command.ExecuteNonQuery();
+                        }
+
+                        // Вывод содержимого таблиц
+                        using (var command = new SQLiteCommand("SELECT Сотрудники.Id, Сотрудники.Name, Сотрудники.Position, Экскурсии.Name AS ExcursionName FROM Сотрудники LEFT JOIN Экскурсии ON Сотрудники.ExcursionId = Экскурсии.Id", connection))
+                        {
+                            using (var reader = command.ExecuteReader())
+                            {
+                                Console.WriteLine("Сотрудники:");
+                                while (reader.Read())
+                                {
+                                    int id = reader.GetInt32(0);
+                                    string name = reader.GetString(1);
+                                    string position = reader.GetString(2);
+                                    string excursionName = reader.IsDBNull(3) ? "No Excursion" : reader.GetString(3);
+                                    Console.WriteLine($"Id: {id}, Name: {name}, Position: {position}, Excursion: {excursionName}");
+                                }
+                            }
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine("Введите Id сотрудника");
+                        int num = Convert.ToInt16(Console.ReadLine());
+
+                        // Удаление элемента из таблицы "Сотрудники"
+                        using (var command = new SQLiteCommand("DELETE FROM Сотрудники WHERE Id = @Id", connection))
+                        {
+                            command.Parameters.AddWithValue("@Id", num);
+                            command.ExecuteNonQuery();
+                        }
+
+                        // Вывод содержимого таблиц
+                        using (var command = new SQLiteCommand("SELECT Сотрудники.Id, Сотрудники.Name, Сотрудники.Position, Экскурсии.Name AS ExcursionName FROM Сотрудники LEFT JOIN Экскурсии ON Сотрудники.ExcursionId = Экскурсии.Id", connection))
+                        {
+                            using (var reader = command.ExecuteReader())
+                            {
+                                Console.WriteLine("Сотрудники:");
+                                while (reader.Read())
+                                {
+                                    int id = reader.GetInt32(0);
+                                    string name = reader.GetString(1);
+                                    string position = reader.GetString(2);
+                                    string excursionName = reader.IsDBNull(3) ? "No Excursion" : reader.GetString(3);
+                                    Console.WriteLine($"Id: {id}, Name: {name}, Position: {position}, Excursion: {excursionName}");
+                                }
+                            }
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("Досвидания");
+                        break;
+                    default:
+                        Console.WriteLine("Неправильный выбор.");
+                        break;
+                }
+                
+
+                
+
+            }
+
+            Console.WriteLine("\nНажмите любой кнопку для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
